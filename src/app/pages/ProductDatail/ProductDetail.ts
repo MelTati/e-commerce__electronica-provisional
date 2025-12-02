ddb15b001b2d9cd0a658dbf3de7263c147cfd92f
MelTati <melanie.matus.salazar.cetis138@gmail.com>
2025-12-01 18:54:45 -0600

    se añadio las rutas de las imagenes

--- src/app/pages/ProductDatail/ProductDetail.ts
+++ src/app/pages/ProductDatail/ProductDetail.ts
@@ -60,8 +60,43 @@ export class ProductDetailComponent implements OnInit {
         next: (data) => {
           this.product.set(data);
 
-          // Reemplazar por imágenes reales si existen
-          this.galleryImages.set([
+          // Selección de imágenes externas según el nombre del producto
+          const nombre = data.fldNombre || '';
+          const galleryMap: Record<string, string[]> = {
+            '555': [
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQBKyeQ56LvXTJVPHr1BTWNYAS6swzezmNk4DkMeX5c2dYw?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQBniBJdV-ThQ6P9zjjhkaLlAcPa7YbsRNAzwZUJwEX7AIc?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQDETcwCIgP9S4Gd_C6SWPPLAe8l86Ad0R6BaUQ0x5-BA9o?download=1'
+            ],
+            '10uF': [
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQD1nSU8zRXZRZM0RsYyVk40AYCgxajeJlWSrNZ0EyVH3rA?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQAzN5SXuW53TIQ4WPzkh1iSAZpR-1wShDcFqowEU2kW03w?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQBCuP7djt1XR5LLuEZq7qu-ATQPQ5sH-YNYciUEDQvodKM?download=1'
+            ],
+            '100uF': [
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQBxX8M40m94QJfmYzLu7_nUARWbbHrU2ZLMo5D1Q-fB9Kw?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQDhPQnX0CYfTatGt4aPK_NaARIxBUnQqT2rrR9d2odiP1M?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQDURpF00IJ3TZ0J4M52RwI2Ae76UX5xdnGjXChYPcI7TU8?download=1'
+            ],
+            '1N4007': [
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQDYrodsIvyZRKkvQVvQ7ILaAUqHhSTg7NTxJEp2wDNDkO8?download=1',
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQA0U0ynqS2dSITCTSjvjwWEATty7DvLJLeXjrx22RjdZ-0?download=1',
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQA5b64TloIxSbFNDUhk6oxIAdKsP124Rv4FAe-K6OYbdIk?download=1'
+            ],
+            'LED rojo': [
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQCAmk2l6wRMTZMoDIh1zg4lAbCEMtR2w5m_iI6MahQiDhM?download=1',
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQAElwE9OXH4SLgEsC-fvY1cARQHGPEPat2a4xg0KY7bq8E?download=1',
+              'https://tgutierrez-my.sharepoint.com/:i:/g/personal/l23270611_tuxtla_tecnm_mx/IQDhsz_bxQiuT5PwOzIRQ0f9AUi_OoLJTQaIyky9DP8poRU?download=1'
+            ],
+            'LED verde': [
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQBpWyJIy869RoZI6T3RPInUAc1oyOsyrulqytkdAxXTlvU?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQDRToak0TvQS4gqiM4nxnmEAbJOILRY0n2eECg1FkGqtWM?download=1',
+              'https://tgutierrez-my.sharepoint.com/:u:/g/personal/l23270611_tuxtla_tecnm_mx/IQASFBNXWYERQ6Q7KhsK1-A4ASuWi29OLobkLo4g-LvvF7M?download=1'
+            ]
+          };
+
+          let selectedGallery = Object.keys(galleryMap).find(key => nombre.includes(key));
+          this.galleryImages.set(selectedGallery ? galleryMap[selectedGallery] : [
             'assets/img/no-image.jpg',
             'assets/img/no-image.jpg',
             'assets/img/no-image.jpg'
@@ -170,41 +205,39 @@ export class ProductDetailComponent implements OnInit {
   // -------------------- CARRITO --------------------
 
   addToCart(codigo_producto: number) {
-  const prod = this.product();
-  if (!prod) return;
+    const prod = this.product();
+    if (!prod) return;
 
-  const idventas = this.cartService.currentCartId();
+    const idventas = this.cartService.currentCartId();
 
-  if (!idventas) {
-    this.cartService.crearCarrito("9614309950", 1).subscribe({
-      next: (res) => {
-        this.cartService.currentCartId.set(res.idventas);
-        localStorage.setItem("venta_id", String(res.idventas));
-        this.addOrUpdateProduct(res.idventas, codigo_producto);
-      },
-      error: () => {
-        this.snack.open('No se pudo crear el carrito', 'Cerrar', { duration: 2500 });
-      }
-    });
-  } else {
-    this.addOrUpdateProduct(idventas, codigo_producto);
+    if (!idventas) {
+      this.cartService.crearCarrito("9614309950", 1).subscribe({
+        next: (res) => {
+          this.cartService.currentCartId.set(res.idventas);
+          localStorage.setItem("venta_id", String(res.idventas));
+          this.addOrUpdateProduct(res.idventas, codigo_producto);
+        },
+        error: () => {
+          this.snack.open('No se pudo crear el carrito', 'Cerrar', { duration: 2500 });
+        }
+      });
+    } else {
+      this.addOrUpdateProduct(idventas, codigo_producto);
+    }
   }
-}
 
   private addOrUpdateProduct(idventas: number, codigo_producto: number) {
-  this.cartService.addOrUpdateProduct(idventas, codigo_producto, this.cantidad()).subscribe({
-    next: () => {
-      // Mostrar notificación
-      this.snack.open('Producto agregado al carrito', 'Cerrar', { duration: 2000 });
-      // Redirigir automáticamente al carrito después de un pequeño delay
-      setTimeout(() => {
-        this.router.navigate(['/carrito']);
-      }, 300); // 300ms para que el snack se vea antes de navegar
-    },
-    error: () => {
-      this.snack.open('Error al agregar el producto', 'Cerrar', { duration: 2500 });
-    }
-  });
-}
+    this.cartService.addOrUpdateProduct(idventas, codigo_producto, this.cantidad()).subscribe({
+      next: () => {
+        this.snack.open('Producto agregado al carrito', 'Cerrar', { duration: 2000 });
+        setTimeout(() => {
+          this.router.navigate(['/carrito']);
+        }, 300);
+      },
+      error: () => {
+        this.snack.open('Error al agregar el producto', 'Cerrar', { duration: 2500 });
+      }
+    });
+  }
 
 }
