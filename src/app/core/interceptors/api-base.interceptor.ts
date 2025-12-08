26508db7b68f26376b085236781a421f7ed9c62b
MelTati <melanie.matus.salazar.cetis138@gmail.com>
2025-12-08 15:29:09 -0600

    cambios de configuracion

--- src/app/core/interceptors/api-base.interceptor.ts
+++ src/app/core/interceptors/api-base.interceptor.ts
@@ -1,3 +1,4 @@
+// core/interceptors/api-base.interceptor.ts
 import { HttpInterceptorFn } from '@angular/common/http';
 import { API_BASE_URL } from '../api-config'; 
 
@@ -5,12 +6,14 @@ export const ApiBaseInterceptor: HttpInterceptorFn = (req, next) => {
   
   if (
     !req.url.startsWith('http://') && 
-    !req.url.startsWith('https://') &&
-    req.url.startsWith('/') 
+    !req.url.startsWith('https://') 
   ) {
+    
+    const baseUrl = API_BASE_URL.endsWith('/') ? API_BASE_URL.slice(0, -1) : API_BASE_URL;
+    const path = req.url.startsWith('/') ? req.url : '/' + req.url;
 
-    const newUrl = API_BASE_URL + req.url;
-    
+    const newUrl = baseUrl + path;
+
     req = req.clone({
       url: newUrl
     });
